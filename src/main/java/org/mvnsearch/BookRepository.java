d1893a3c56fcf9b658b0bd64a08f06267334c5c6
linux_china <libing.chen@gmail.com>
2024-04-20 22:33:58 +0800

    chore: extends CrudRepository

--- src/main/java/org/mvnsearch/BookRepository.java
+++ src/main/java/org/mvnsearch/BookRepository.java
@@ -1,11 +1,12 @@
 package org.mvnsearch;
 
+import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Find;
 import jakarta.data.repository.Repository;
 import org.hibernate.StatelessSession;
 
 @Repository
-public interface BookRepository {
+public interface BookRepository extends CrudRepository<Book, Integer> {
     StatelessSession session();
 
     @Find
